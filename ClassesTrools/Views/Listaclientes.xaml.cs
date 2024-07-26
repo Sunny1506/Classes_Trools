6ec50fcd82a03467a593ee83a2243befddd9e7af
Manu <emanuelebrumrossi0@gmail.com>
2024-07-26 08:58:41 -0300

    continuação da interface

---
+++ ClassesTrools/Views/Listaclientes.xaml.cs
@@ -0,0 +1,14 @@
+using Microsoft.Maui.Controls;
+
+namespace ClassesTrools.Views
+{
+    public partial class Listaclientes
+    {
+        public Listaclientes()
+        {
+            InitializeComponent();
+        }
+
+        // Add event handlers and logic here if needed
+    }
+}
