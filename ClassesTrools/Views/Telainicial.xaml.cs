6599e892050d8efe25263dac4062e53e1e53bc1c
Manu <emanuelebrumrossi0@gmail.com>
2024-07-29 08:57:41 -0300

    fazendo mais telas...

---
+++ ClassesTrools/Views/Telainicial.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.Maui.Controls;
+
+namespace ClassesTrools
+{
+    public partial class Telainicial 
+    {
+        public Telainicial()
+        {
+            InitializeComponent();
+        }
+    }
+}
